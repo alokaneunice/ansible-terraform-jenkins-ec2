0decff84f2256f98a550441eb95431ca0e2e5a7b
alokaneunice <alokaneunice@outlook.com>
2025-07-25 12:41:23 +0100

    terraform commit

--- vpc.tf
+++ vpc.tf
@@ -12,6 +12,23 @@ resource "aws_subnet" "myapp-subnet" {
     Name = "${var.env_prefix}-subnet"
   }
 }
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
 resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
